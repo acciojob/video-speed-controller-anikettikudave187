72b9433b29de1aaabfb012e2ec7abd2dbf56b1b1
Aniket Tikudave <anikettikudave187@gmail.com>
2025-07-03 17:21:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+const video=document.querySelector("video");
+const speed=document.querySelector(".speed");
+const speedBar=document.querySelector(".speed-bar");
+
+function togglePlay() {
+	video.paused ? video.play() :video.pause();
+}
+
+function updateButton() {
+	speedBar.textContent=video.paused ? '► ' :'❚ ❚';
+}
+
+function handleSpeed(e){
+	const y=e.offsetY;
+	const percent=y/speed.offsetHeight;
+	const min=0.4;
+	const max=4;
+	const height=`${percent*100}%`;
+	const playbackRate = percent * (max - min) + min;
+	  speedBar.style.height = height;
+	  speedBar.textContent = playbackRate.toFixed(2) + '×';
+	  video.playbackRate = playbackRate;
+}
+
+video.addEventListener("click",togglePlay);
+video.addEventListener("play",updateButton);
+video.addEventListener("pause",updateButton);
+speed.addEventListener("mousemove",handleSpeed);
+
+
+
+
+
+
+
+
